4e4cb6846535bc7e64a478f4286c72e75657b4a5
yangli <li002252@umn.edu>
2022-05-20 09:45:13 -0500

    add dependencies

--- cmake/htslib.cmake
+++ cmake/htslib.cmake
@@ -1,40 +1,26 @@
 include(ExternalProject)
+
+set(htslib_PREFIX ${CMAKE_BINARY_DIR}/htslib-prefix)
+set(htslib_INSTALL ${CMAKE_BINARY_DIR}/htslib-install)
+
 if (CMAKE_GENERATOR STREQUAL "Unix Makefiles")
-    # when using the makefile generator, use the special variable $(MAKE) to invoke make
-    # this enables the jobserver to work correctly
     set(MAKE_COMMAND "$(MAKE)")
-else ()
-    # invoke make explicitly
-    # in this case, we assume the parent build system is running in parallel already so no -j flag is added
+else()
     find_program(MAKE_COMMAND NAMES make gmake)
-endif ()
+endif()
 
-if (INSTALL_DEPENDENCIES)
-    set(HTSLIB_INSTALL ${MAKE_COMMAND} install prefix=${CMAKE_INSTALL_PREFIX})
-else ()
-    set(HTSLIB_INSTALL "")
-endif ()
-
-
-# build htslib
-set(htslib_PREFIX ${CMAKE_BINARY_DIR}/htslib)
-
-externalproject_add(
-        htslib
-        BUILD_IN_SOURCE 1
-        URL https://github.com/samtools/htslib/releases/download/1.15.1/htslib-1.15.1.tar.bz2
+ExternalProject_Add(htslib
         PREFIX ${htslib_PREFIX}
-        CONFIGURE_COMMAND autoconf && ./configure --prefix=${CMAKE_BINARY_DIR}/htslib --disable-lzma --disable-s3 --disable-plugins --disable-bz2 --disable-libcurl
-        BUILD_COMMAND make CFLAGS=${CMAKE_C_FLAGS}
-        INSTALL_COMMAND "${HTSLIB_INSTALL}"
-#        LOG_DOWNLOAD 0
-#        LOG_UPDATE 0
-#        LOG_CONFIGURE 0
-#        LOG_BUILD 0
-#        LOG_TEST 0
-#        LOG_INSTALL 0
-)
+        URL https://github.com/samtools/htslib/releases/download/1.15.1/htslib-1.15.1.tar.bz2
+        BUILD_IN_SOURCE 1
+        UPDATE_COMMAND ""
+        CONFIGURE_COMMAND autoconf && ./configure --prefix=${htslib_PREFIX} --disable-lzma --disable-s3 --disable-plugins --disable-bz2 --disable-libcurl
+        BUILD_COMMAND ${MAKE_COMMAND} lib-static
+        INSTALL_COMMAND ${MAKE_COMMAND} install prefix=${htslib_INSTALL}
+        )
 
-externalproject_get_property(htslib SOURCE_DIR)
-set(HTSLIB_SRC_DIR ${SOURCE_DIR})
-include_directories("${HTSLIB_SRC_DIR}/htslib")
+include(zlib)
+
+add_dependencies(htslib zlib)
+include_directories(${htslib_INSTALL}/include/htslib)
+set(htslib_LIB ${htslib_INSTALL}/lib/libhts.a ${zlib_LIB} ${CMAKE_THREAD_LIBS_INIT})
