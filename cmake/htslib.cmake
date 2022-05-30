f3d8fbe179ce43cd7bec87159e9a9031f9c9c489
yangli <li002252@umn.edu>
2022-05-29 23:38:05 -0500

    refactor htslib.cmake

--- cmake/htslib.cmake
+++ cmake/htslib.cmake
@@ -15,13 +15,15 @@ ExternalProject_Add(htslib
         URL https://github.com/samtools/htslib/releases/download/1.15.1/htslib-1.15.1.tar.bz2
         BUILD_IN_SOURCE 1
         UPDATE_COMMAND ""
-        CONFIGURE_COMMAND autoconf && ./configure --prefix=${htslib_PREFIX} --disable-lzma --disable-s3 --disable-plugins --disable-bz2 --disable-libcurl
+        CONFIGURE_COMMAND autoconf && ./configure --prefix=${htslib_PREFIX}  --disable-bz2 --disable-lzma --disable-gcs --disable-s3 --disable-plugins --disable-libcurl
         BUILD_COMMAND ${MAKE_COMMAND} lib-static
         INSTALL_COMMAND ${MAKE_COMMAND} install prefix=${htslib_INSTALL}
         )
 
 include(zlib)
-
 add_dependencies(htslib zlib)
 include_directories(${htslib_INSTALL}/include/htslib)
-set(htslib_LIB ${htslib_INSTALL}/lib/libhts.a ${zlib_LIB} ${CMAKE_THREAD_LIBS_INIT})
+set(htslib_LIB ${htslib_INSTALL}/lib/libhts.a
+    ${zlib_LIB}
+    ${CMAKE_THREAD_LIBS_INIT}
+    )
