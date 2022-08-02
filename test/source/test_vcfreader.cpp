925f88e9146cb3f4e6c4db3f76220183ae4932c7
yangli <li002252@umn.edu>
2022-08-02 11:14:06 -0500

    use smart ptr in vcfreader and finished test case

--- test/source/test_vcfreader.cpp
+++ test/source/test_vcfreader.cpp
@@ -15,7 +15,13 @@ TEST_CASE("testing vcf reader") {
 
   SUBCASE("testing  query") {
     CHECK_FALSE(reader.has_index());
-    reader.query("chr10", 93547288, 93867288);
+
+    for (auto i = reader.query("chr17", 7707250, 7798250); i != reader.end();
+         i = reader.iter_query_record()) {
+      auto [chrom, pos] = *i;
+      spdlog::info("Chrom: {} Pos: {} SVTYPE: {} SVEND: {}", chrom, pos,
+                   get_info_field_string("SVTYPE", i), get_info_field_int32("SVEND", i));
+    }
   }
 
   SUBCASE("testing read record") {
@@ -23,18 +29,18 @@ TEST_CASE("testing vcf reader") {
     // chr10   93567288        1       .       <TRA>
     // .       .       CANONICAL;BOUNDARY=NEITHER;SVTYPE=TRA;
     // SR=2;OSR=1;CHR2=chr17;SVEND=7705262
-    reader.next_record();
-    CHECK_EQ(reader.get_chrom(), "chr10");
-    CHECK_EQ(reader.get_pos(), 93567288 - 1);
+    auto begin_record = reader.begin();
+    CHECK_EQ(begin_record.get_chrom(), "chr10");
+    CHECK_EQ(begin_record.get_pos(), 93567288 - 1);
     // 0-based
-    CHECK_EQ(reader.get_info_int("SVEND"), 7705262);
-    CHECK_EQ(reader.get_info_string("SVTYPE"), "TRA");
-    CHECK_EQ(get_info_field<int32_t>("SVEND", reader), 7705262);
+    CHECK_EQ(get_info_field_int32("SVEND", begin_record), 7705262);
   }
 
   SUBCASE("testing read all record") {
-    while (reader.next_record() >= 0) {
-      reader.check_record();
+    for (auto i = reader.begin(); i != reader.end(); ++i) {
+      auto [chrom, pos] = *i;
+      spdlog::info("Chrom: {} Pos: {} SVTYPE: {} SVEND: {}", chrom, pos,
+                   get_info_field_string("SVTYPE", i), get_info_field_int32("SVEND", i));
     }
   }
 }
