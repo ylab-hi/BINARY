edfcf0e8a6b197db18aeca1af5e56de5873073f6
yangli <li002252@umn.edu>
2022-08-02 09:05:49 -0500

    add begin and end to VcfReader

--- library/include/sv2nl/vcf_reader.hpp
+++ library/include/sv2nl/vcf_reader.hpp
@@ -8,6 +8,7 @@
 #include <string>
 #include <sv2nl/exception.hpp>
 #include <sv2nl/info_field.hpp>
+#include <tuple>
 #include <utility>
 
 #include "htslib/tbx.h"
@@ -17,6 +18,11 @@ namespace sv2nl {
   void bcf_record_deleter(bcf1_t* record) noexcept;
   void bcf_hdr_deleter(bcf_hdr_t* hdr) noexcept;
   void bcf_hts_file_deleter(htsFile* hts_file) noexcept;
+  void bcf_tbx_deleter(tbx_t* tbx) noexcept;
+  void bcf_itr_deleter(hts_itr_t* itr) noexcept;
+  void bcf_kstring_deleter(kstring_t* ks) noexcept;
+
+  class VcfRecord;
 
   class VcfReader {
   private:
@@ -51,6 +57,10 @@ namespace sv2nl {
     [[nodiscard]] auto get_info_int(const std::string& key) const -> int32_t;
     [[nodiscard]] auto get_info_string(const std::string& key) const -> std::string;
 
+    auto begin() -> VcfRecord&;
+    auto begin() const -> VcfRecord const&;
+    auto end() const -> VcfRecord;
+
     auto next_record() -> int;
     void print_record() const;
 
@@ -62,6 +72,13 @@ namespace sv2nl {
   public:
     friend class VcfReader;
     friend auto operator<<(std::ostream& os, VcfRecord const& record) -> std::ostream&;
+
+    template <typename DataType>
+    friend auto get_info_field(const std::string& key, VcfRecord const& vcf_record) ->
+        typename InfoField<DataType>::result_type;
+
+    using iterator_category = std::forward_iterator_tag;
+    using value_type = std::tuple<std::string, int64_t>;
 
     VcfRecord() = default;
     explicit VcfRecord(std::shared_ptr<htsFile> file)
@@ -77,18 +94,40 @@ namespace sv2nl {
     auto operator=(VcfRecord&&) noexcept -> VcfRecord& = default;
 
     void check_header() const;  // will throw if header is not set
-    // all functions is unchecked for header pay attentions
+    // WARNING: all functions are unchecked for header pay attentions
     [[nodiscard]] auto get_record() const -> bcf1_t*;
     [[nodiscard]] auto get_header() const -> bcf_hdr_t*;
     [[nodiscard]] auto get_chrom() const -> std::string;
     [[nodiscard]] auto get_pos() const -> int64_t;
     [[nodiscard]] auto get_rlen() const -> int64_t;
     [[nodiscard]] auto is_valid() const -> bool;
+    void clear_record();
 
-  private:
+    // overload operators
+    friend auto operator==(VcfRecord const& lhs, VcfRecord const& rhs) -> bool;
+
+    auto operator++() -> VcfRecord&;
+    auto operator++(int) -> VcfRecord;
+    auto operator*() const -> value_type;
+
+  protected:
     std::shared_ptr<htsFile> file_{nullptr, bcf_hts_file_deleter};
     std::shared_ptr<bcf_hdr_t> header_{nullptr, bcf_hdr_deleter};
     std::shared_ptr<bcf1_t> record_{bcf_init(), bcf_record_deleter};
+  };
+
+  class VcfQueryRecord : public VcfRecord {
+  public:
+    using VcfRecord::VcfRecord;
+    VcfQueryRecord() = default;
+
+    auto operator++() -> VcfQueryRecord&;
+    auto operator++(int) -> VcfQueryRecord;
+
+  private:
+    std::shared_ptr<hts_itr_t> itr_{nullptr, bcf_itr_deleter};
+    std::shared_ptr<tbx_t> tbx_{nullptr, bcf_tbx_deleter};
+    std::shared_ptr<kstring_t> ks_{nullptr, bcf_kstring_deleter};
   };
 
   template <typename DataType>
@@ -109,6 +148,34 @@ namespace sv2nl {
     int32_t count{};
 
     if (int ret = bcf_get_info_values(vcf_reader.get_header(), vcf_reader.get_record(), key.c_str(),
+                                      (void**)(&data), &count, data_id);
+        ret < 0) {
+      throw VcfReaderError("Failed to get info " + key);
+    }
+
+    typename InfoField<DataType>::result_type result = info_field.get_result(data, count);
+    free(data);
+    return result;
+  }
+
+  template <typename DataType>
+  auto get_info_field(const std::string& key, VcfRecord const& vcf_record) ->
+      typename InfoField<DataType>::result_type {
+    /**
+ #define BCF_HT_FLAG 0  header type
+ #define BCF_HT_INT  1
+ #define BCF_HT_REAL 2
+ #define BCF_HT_STR  3
+ #define BCF_HT_LONG (BCF_HT_INT | 0x100)  BCF_HT_INT, but for int64_t values; VCF only!
+ **/
+
+    auto info_field = InfoField<DataType>{};
+    auto data_id = info_field.data_id;
+
+    DataType* data{nullptr};
+    int32_t count{};
+
+    if (int ret = bcf_get_info_values(vcf_record.get_header(), vcf_record.get_record(), key.c_str(),
                                       (void**)(&data), &count, data_id);
         ret < 0) {
       throw VcfReaderError("Failed to get info " + key);
